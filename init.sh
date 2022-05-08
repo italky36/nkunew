2f8c21e8d632b044d132ed8eabd7a9464c60e1c7
Artyom Shkarlatuk <skarlatuka@gmail.com>
2022-05-09 00:07:51 +0300

    change env

--- init.sh
+++ init.sh
@@ -18,109 +18,7 @@ chmod +x /usr/local/bin/docker-compose
 touch acme.json
 chmod 600 acme.json
 
-read -p "Введите пароль от traefik: " traefiks
-traefikp=`htpasswd -nb admin $traefiks`
-
-read -p "Введите почту для сертификата: " mail
-
-read -p "Введите домен: " domain
-
-read -p "Введите пароль от wireguard: " vpnpass
-
-echo "Генерация конфигов"
-
-echo -en "defaultEntryPoints = ["\"http\"", "\"https\""]
-
-[entryPoints]
- [entryPoints.dashboard]
-   address = "\":8080\""
-   [entryPoints.dashboard.auth]
-     [entryPoints.dashboard.auth.basic]
-       users = ["\"$traefikp\""]
- [entryPoints.http]
-   address = "\":80\""
-     [entryPoints.http.redirect]
-       entryPoint = "\"https\""
- [entryPoints.https]
-   address = "\":443\""
-     [entryPoints.https.tls]
-
-[api]
-entrypoint="\"dashboard\""
-
-[acme]
- email = "\"$mail\""
- storage = "\"acme.json\""
- entryPoint = "\"https\""
- onHostRule = true
-   [acme.httpChallenge]
-   entryPoint = "\"http\""
-
-[[acme.domains]]
-  main = "\"$domain\""
-  sans = ["\"www.$domain\"", "\"vpnsec.$domain\"","\"monitor.$domain\""]   
-
-[docker]
- domain = "\"$domain\""
- watch = true
- network = "\"web\""
-" > traefik.toml
-
-
-echo -en "version: '3'
-
-services:
-  nodejs:
-    build:
-      context: .
-      dockerfile: Dockerfile
-    image: nodejs
-    container_name: nodejs
-    restart: unless-stopped
-    labels:
-      - traefik.backend=blog
-      - traefik.frontend.rule=Host:$domain,www.$domain
-      - traefik.docker.network=web
-      - traefik.port=3000
-    volumes:
-      - ./db:/home/node/app/db/
-    networks:
-      - web
-
-  wg-easy:
-    environment:
-      - WG_HOST=$domain
-      - PASSWORD=$vpnpass
-      - WG_PORT=51820
-      - WG_DEFAULT_ADDRESS=10.0.0.x
-      - WG_DEFAULT_DNS=1.1.1.1
-      - WG_MTU=1420
-      - WG_ALLOWED_IPS=10.0.0.0/24
-    image: weejewel/wg-easy
-    container_name: wg-easy
-    volumes:
-      - ./wireguard:/etc/wireguard
-    ports:
-      - "\"51820:51820/udp\""
-      - "\"51821:51821/tcp\""
-    restart: unless-stopped
-    labels:
-      - traefik.backend=vpnsec
-      - traefik.frontend.rule=Host:vpnsec.$domain
-      - traefik.docker.network=web
-      - traefik.port=51821
-    cap_add:
-      - NET_ADMIN
-      - SYS_MODULE
-    sysctls:
-      - net.ipv4.ip_forward=1
-      - net.ipv4.conf.all.src_valid_mark=1
-    networks:
-      - web
-
-networks:
-  web:
-    external: true" >  docker-compose.yml
+traefikpass=`htpasswd -nb admin $traefikpass`
 
 docker network create web
 
@@ -135,7 +33,7 @@ docker run -d --restart unless-stopped \
   --network web \
   --name traefik \
   traefik:1.7.2-alpine
-  
+
 docker-compose up -d
 
 echo "Firewall setup"
